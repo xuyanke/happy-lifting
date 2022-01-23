636a7f77aa781fea6c1b87438f58eeb6067c5524
Xuyan Ke <kexuyan@live.cn>
2022-01-22 18:41:23 -0800

    Add support to set plate weights

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -3,11 +3,35 @@ var app = window.app || {};
 (function(){
   'use strict';
 
+  var Utils = app.Utils;
+
   app.Settings = React.createClass({
+
+    getInitialState: function() {
+      var plates = Utils.plates();
+      var platesStr = plates.map(String).join(',');
+      return {
+        platesStr: platesStr,
+      };
+    },
+
+    handlePlateChoicesChange: function(event) {
+      var platesStr = event.target.value;
+      Utils.savePlatesChoices(platesStr);
+      this.setState({
+        platesStr: platesStr
+      });
+
+    },
+
     render: function() {
+      var platesStr = this.state.platesStr;
       return (
         <div>
-          settings   
+          <div className="form-group">
+            <label className="control-label">Plate Choices</label>
+            <input type="text" className="form-control" style={{fontSize: '16px'}} onChange={this.handlePlateChoicesChange} value={platesStr}/>
+          </div>
         </div>
       );
     }
